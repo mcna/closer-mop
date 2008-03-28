48419d000d48cb4845d6e0fd62f502b831cce366
pc <pc@p-cos.net>
2008-03-28 10:28:17 -0700

    Checked against clisp 2.44.1 - no changes.
    darcs-hash:20080328172817-dccf3-c8d8559c466dcbdded52a3130ab796e5cf3fabd8.gz

--- features.lisp
+++ features.lisp
@@ -34,7 +34,7 @@
  (:subclasses-of-standard-writer-method-do-not-inherit-exported-slots)
  (:t-is-always-a-valid-superclass))
 
-:clisp2.43
+:clisp2.44.1
 ((:accessor-method-initialized-with-function)
  (:add-method-calls-compute-discriminating-function)
  (:compute-slots-requested-slot-order-honoured)
