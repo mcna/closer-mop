d1342ef3e0a2f1fcda44804eeea561cbb2f07baa
pc <pc@p-cos.net>
2008-06-29 22:25:01 -0700

    Checked against SBCL 1.0.18 - no changes.
    darcs-hash:20080630052501-dccf3-abab205b541650b1ad4a7a604faebae1ed589f9b.gz

--- features.lisp
+++ features.lisp
@@ -417,5 +417,5 @@
  (:subclasses-of-standard-accessor-method-do-not-inherit-exported-slots)
  (:subclasses-of-standard-writer-method-do-not-inherit-exported-slots))
 
-:sbcl 1.0.17
+:sbcl 1.0.18
 #| all features implemented |#
