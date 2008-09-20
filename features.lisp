452c0c77dc23d9ab4a87f144e4d4027083f595d0
pc <pc@p-cos.net>
2008-09-20 07:32:17 -0700

    Checked against CCL 1.2 - no changes.
    darcs-hash:20080920143217-dccf3-2cff67e93a4a60b15a0877ede4a6d89073260523.gz

--- features.lisp
+++ features.lisp
@@ -48,7 +48,7 @@
  (:remove-method-calls-compute-discriminating-function)
  (:subclasses-of-method-combination-do-not-inherit-exported-slots))
 
-:clozure-common-lisp1.2-rc1
+:clozure-common-lisp1.2
 ((:compute-applicable-methods-using-classes)
  (:defmethod-calls-generic-function-method-class)
  (:defmethod-calls-make-method-lambda)
