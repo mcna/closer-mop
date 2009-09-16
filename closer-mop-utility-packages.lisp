08763bb99a5845567f313622dc2083482ad97ed4
pc <pc@p-cos.net>
2009-09-16 03:44:28 -0700

    A defpackage form has to fully describe its state, which was not the case for CLOSER-COMMON-LISP. Now fixed, due to Tobias C. Rittweiler.
    darcs-hash:20090916104428-dccf3-87a5cfdd6fe1e4cc4b97048be25cc45669e40b60.gz

--- closer-mop-utility-packages.lisp
+++ closer-mop-utility-packages.lisp
@@ -1,17 +1,24 @@
 (in-package :cl-user)
 
-(defpackage #:closer-common-lisp
-  (:nicknames #:c2cl)
-  (:use))
-
-(let ((syms (nunion (loop for sym being the external-symbols of :common-lisp
-                          if (find-symbol (symbol-name sym) :c2mop)
-                          collect it
-                          else collect sym)
-                    (loop for sym being the external-symbols of :c2mop
-                          collect sym))))
-  (import syms :c2cl)
-  (export syms :c2cl))
+(macrolet ((define-closer-common-lisp-package ()
+             (loop with symbols = (nunion (loop for sym being the external-symbols of :common-lisp
+                                                if (find-symbol (symbol-name sym) :c2mop)
+                                                collect it
+                                                else collect sym)
+                                          (loop for sym being the external-symbols of :c2mop
+                                                collect sym))
+                   with map = '()
+                   for symbol in symbols do
+                   (push (symbol-name symbol)
+                         (getf map (symbol-package symbol)))
+                   finally (return 
+                            `(defpackage #:closer-common-lisp
+                               (:nicknames #:c2cl)
+                               (:use)
+                               ,@(loop for (package symbols) on map by #'cddr
+                                       collect `(:import-from ,(package-name package) ,@symbols))
+                               (:export ,@(mapcar #'symbol-name symbols)))))))
+  (define-closer-common-lisp-package))
 
 (defpackage #:closer-common-lisp-user
   (:nicknames #:c2cl-user)
