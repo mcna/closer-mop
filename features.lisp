26de3e846afbf5aafa57e7a2cb8d40056425bf8a
pc <pc@p-cos.net>
2010-04-11 13:29:09 -0700

    Checked against ECL 10.4.1 - no changes.
    Ignore-this: dcae03d73f4e136c2403f3101166c397
    
    darcs-hash:20100411202909-dccf3-5e41a66c8c70a89e100c2a80905c27f85a56eed5.gz

--- features.lisp
+++ features.lisp
@@ -123,7 +123,7 @@
  (:subclasses-of-standard-slot-definition-do-not-inherit-exported-slots)
  (:subclasses-of-standard-writer-method-do-not-inherit-exported-slots))
 
-:ecl10.3.1
+:ecl10.4.1
 ((:add-direct-method fixed)
  (:add-method-calls-add-direct-method fixed)
  (:add-method-calls-compute-discriminating-function fixed)
