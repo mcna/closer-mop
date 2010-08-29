68cdf4654061887597bcc0fb13a1a46232607ee3
pc <pc@p-cos.net>
2010-08-29 15:39:15 -0700

    Checked against SBCL 1.0.42 - no changes.
    Ignore-this: 646eaa67130257fe85f7d54473643faf
    
    darcs-hash:20100829223915-dccf3-cbe4da6e8fe44641202bc2db4e6106977822896e.gz

--- features.lisp
+++ features.lisp
@@ -370,7 +370,7 @@
  (:subclasses-of-standard-slot-definition-do-not-inherit-exported-slots)
  (:subclasses-of-standard-writer-method-do-not-inherit-exported-slots))
 
-:sbcl1.0.41
+:sbcl1.0.42
 #| all features implemented |#
 
 :scl1.3.9
